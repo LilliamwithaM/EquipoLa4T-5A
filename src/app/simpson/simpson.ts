c1f08347201d9d3be8f2a285ac9b2ff2b42f1f46
LuisCocotleUV <luisdavidcocoyuv@gmail.com>
2022-11-11 10:20:37 -0600

    simpson Closes #2

--- src/app/simpson/simpson.ts
+++ src/app/simpson/simpson.ts
@@ -1,98 +1,56 @@
-*********simpson.ts*****************
+import { fx_1eX } from "./f(x)=1ex";
+import { fx_2x } from "./f(x)=2x";
+import { fx_XxX } from "./f(x)=XxX";
 
-    import { func2x } from "./func2x";
-    import { funcx2 } from "./funcx2";
-    import { func1x } from "./func1x";
-
-    export function simpson(x0:number, x1:number, num_seg:number,op:string){
-
-    let w = (x1-x0)/num_seg;
-    let wDiv3 = w/3;
-    let x = 0, i = 0, y = 0;
-    let area;
-    let integral = 0;
-    console.log("esto vale i al inicio = "+i)
-    do{
-  
-        if(i == 0 || i == num_seg){
-            if(op == 'm'){
-                y = func2x(x) * 1;
-                console.log("func2x");
-            }else if(op == 'd'){
-                console.log("x = " + x);
-                if(x == 0){
-                    y = 0;
-                }else{
-                    y = func1x(x) * 1;
-                }
-                console.log("func1/x " + "y = "+y);
-            }else{
-                y = funcx2(x) * 1;
-                console.log("funcxpow2");
+export function simpson(f: string, x0: number, x1: number, num_seg: number, dof: number) {
+    let E = 0.00001;
+    let x = 0;
+    let xC;
+    let y;
+    let area_seg;
+    let P = 0;
+    let PF;
+    let contador = 0;
+    const arrP = [];
+    let margenE = 1;
+    do {
+        margenE = 0;
+        let W = (x1 - x0) / num_seg;
+        let W3 = W / 3;
+        x = x0;
+        for (let i = 0; i <= num_seg; i++) {
+            if (f.toLowerCase() == "2x") {
+                xC = fx_2x(x)
+            } else if (f.toLowerCase() == "x*x") {
+                xC = fx_XxX(x)
+            } else if (f.toLowerCase() == "1/x") {
+                xC = fx_1eX(x)
+            } else {
+                xC = fx_2x(x)
             }
-                        
-            area = y * wDiv3;
-            console.log("area = " + area + "multiplicador = 1 ");
-        }else if(i%2 == 0){
-            if(op == 'm'){
-                y = func2x(x) * 2;
-                console.log("func2x");
-            }else if(op == 'd'){
-                y = func1x(x) * 2;
-                console.log("func1/x " + "y = "+y);
-            }else{
-                y = funcx2(x) * 2;
-                console.log("funcxpow2");
-            }
-            area = y * wDiv3;
-            console.log("area = " + area + "multiplicador = 2 ");
 
-        }else{
-            if(op == 'm'){
-                y = func2x(x) * 4;
-                console.log("func2x");
-            }else if(op == 'd'){
-                y = func1x(x) * 4;
-                console.log("func1/x " + "y = "+y);
-            }else{
-                y = funcx2(x) * 4;
-                console.log("funcxpow2");
+            if (i == 0 || i == num_seg) {
+                y = xC * 1
+            } else {
+                ((i % 2!) == 0) ? y = xC * 2 : y = xC * 4
             }
-            area = y * wDiv3;
-            console.log("area = " + area + "multiplicador = 4 ");
+            area_seg = y * W3;
+            P += area_seg;
+            x += W;
         }
+        arrP.push(P);
+        num_seg = num_seg * 2;
+        contador += 1;
+        if (contador > 0) {
+            margenE = arrP[0] - arrP[contador];
+        }
+        PF = parseFloat(P.toFixed(5))
+        P = 0;
+    } while (margenE > E || contador < 2)
 
-
-        x = x+w;
-        console.log("esto vale x ahora = "+x);
-        integral = integral + area;
-        i++;
-        console.log("el numero de segmento es = "+ num_seg +" esto vale i ahora =" + i );
-    }while(i<=num_seg);
-
-
-    return integral;
-
+    if (PF == 0) {
+        PF = 1;
     }
 
-************func2x.ts********************
-
-export function func2x(x:number){
-    let result = x*2;
-    return result;
-}
-
-
-
-************func1x.ts********************
-export function func1x(x:number){
-    let resultado = (1/x);
-    return resultado;
-}
-
-************funcx2.ts********************
-
-export function funcx2(x:number){
-    let resultado = x*x;
-    return resultado;
+    return PF
 }
